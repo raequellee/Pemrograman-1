4ae3c17dfbcc6fd20ae1bf3eae0079676e32fb83
raequellee <2410817220030@mhs.ulm.ac.id>
2024-10-20 10:06:06 +0800

    Add files via upload

---
+++ Modul-1/Soal-8/PRAK108-2410817220030-RachelWinaYuda.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    float putaran = 5;
+    float jarak_total = 14;
+    float jarijari = jarak_total / (2 * 3.14 * 5);
+    printf("Diketahui :\n");
+    printf("Pak Dengklek mengelilingi taman = %.f Putaran\n", putaran);
+    printf("Jarak tempuh Pak Dengklek = %.f Kilometer\n", jarak_total);
+    printf("\n");
+    printf("Jawaban :\n");
+    printf("Jari-jari taman yang dikelilingi Pak Dengklek adalah %.2f Kilometer", jarijari);
+    return 0;
+}
